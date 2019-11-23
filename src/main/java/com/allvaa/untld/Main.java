ff34c517eb23e81ffb88e141f8424a130860cd46
Alva <52526068+Allvaa@users.noreply.github.com>
2019-11-23 21:03:00 +0700

    first

---
+++ src/main/java/com/allvaa/untld/Main.java
@@ -0,0 +1,15 @@
+package com.allvaa.untld;
+
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+
+public class Main {
+    public static void main(String[] args) throws LoginException {
+        JDA client = new JDABuilder(AccountType.BOT)
+                .setToken("")
+                .build();
+    }
+}
