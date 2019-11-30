4b2efe7fd9ae6c64069abef4371e84a2753e1eec
Allvaa <alvamailindo@gmail.com>
2019-11-30 21:20:51 +0700

    exec command

--- src/main/java/com/allvaa/untld/Main.java
+++ src/main/java/com/allvaa/untld/Main.java
@@ -8,7 +8,6 @@ import net.dv8tion.jda.api.JDABuilder;
 
 import javax.security.auth.login.LoginException;
 
-
 public class Main {
     public static void main(String[] args) throws LoginException {
         Untld untld = new Untld(new Config());
